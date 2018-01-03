dc0ca10e2a54ddc7f99eaa67eef81ad609613447
Kyle MacNeney <kyle.macneney@gmail.com>
2018-01-02 22:26:48 -0500

    forms change

--- email_app/forms.py
+++ email_app/forms.py
@@ -15,5 +15,13 @@ class NotificationForm(forms.Form):
 	This form should be a row in a table of notifications that a user
 	can manage from a notification table
 	"""
+
+	EVENT_LIST = (
+		(0, "--- None ---"),
+		(1, "Birthday"),
+		(2, "Christmas"),
+	)
+
 	recipient_first_name = forms.CharField(label='Recipient First Name', max_length=100)
 	recipient_relationship = forms.CharField(label='Relationship', max_length=100)
+	event = forms.ChoiceField(label='Event', widget=forms.Select(), choices=self.EVENT_LIST, default=0)
